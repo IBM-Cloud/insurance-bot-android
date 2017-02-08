64ab42f4025a55d8f95b29b46914e65c47b453d4
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-02-08 17:46:21 +0100

    user_time was not passed to the backend - this would lead to incorrect dates depending on the hour of the day #2

--- app/src/main/java/com/ibm/org/vidyasagar/models/ChatRequest.java
+++ app/src/main/java/com/ibm/org/vidyasagar/models/ChatRequest.java
@@ -3,17 +3,34 @@ package com.ibm.org.vidyasagar.models;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 /**
  * Created by Vidyasagar MSC on 28/12/16.
  */
 
 public class ChatRequest {
+
+    private static DateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.000'Z'", Locale.US);
+    static {
+        DATE_FORMATTER.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
+    @SerializedName("user_time")
+    @Expose
+    private String usertime = DATE_FORMATTER.format(new Date());
     @SerializedName("text")
     @Expose
     private String text;
     @SerializedName("context")
     @Expose
     private Context context;
+
+    public String getUsertime() { return usertime; }
 
     public String getText() {
         return text;
